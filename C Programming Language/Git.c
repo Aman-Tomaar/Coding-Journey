708566ec741856ac26261e44678c8ff566feead3
Aman Tomar <choudharyamantomar@gmail.com>
2026-05-14 23:40:42 +0530

    Deleted Git.c file

--- C Programming Language/Git.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello GitHub\n");
-    printf("I have successfully connected my VS Code to GitHub!! \n");
-    printf("Testing gitignore\n");
-    return 0;
-}
