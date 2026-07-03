23bd4560e9f4e34996cb20b638bc96c599f6ccd1
Aman Tomar <choudharyamantomar@gmail.com>
2026-07-03 14:17:49 +0530

    user imput fxn

---
+++ Python Programming Language/Topics/3-0_UserInputFxn.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+print(f"Hyy {name}!\n")
+ 
+age = int(input("Enter your age: "))
+print(f"So you are {age} years old")
+print("HAPPY BIRTHDAY!!")
+age += 1
+print(f"Now your age is {age} years old")
