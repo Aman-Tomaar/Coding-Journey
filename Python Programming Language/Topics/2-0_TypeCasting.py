f91b2a1493ae601e67345d05fbee6f71b5aa3e2f
Aman Tomar <choudharyamantomar@gmail.com>
2026-07-03 13:40:05 +0530

    adding TypeCasting

---
+++ Python Programming Language/Topics/2-0_TypeCasting.py
@@ -0,0 +1,15 @@
+name = "Strange"
+empty_str = ""
+age = 18
+gpa = 9.1
+
+name = bool(name)
+empty_str = bool(empty_str)
+print(f"name : {name}")
+print(f"empty_str : {empty_str}\n")
+
+age = float(age)
+print(f"age {age}" , "\n")
+
+gpa = int(gpa)
+print(f"gpa : {gpa} \n")
