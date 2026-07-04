f805e8e86fcd62d3ca710ec27037ad4e3f21d05a
Aman Tomar <choudharyamantomar@gmail.com>
2026-07-04 10:21:47 +0530

    adding math functions

---
+++ Python Programming Language/Topics/4-0_MathFxn.py
@@ -0,0 +1,9 @@
+x = 3.14
+y = -4
+z = 5
+
+print( round (x) )     # Round of decimal
+print( abs (y) )       # Absolute value (distance of a number from 0)
+print( pow (y,z) )     # Power = y to the power z 
+print( max (x, y, z) ) # Finds the max num from given number
+print( min (x, y, z) ) # Finds the min num from given number
