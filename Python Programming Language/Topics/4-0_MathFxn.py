4e8393d2b482f2317065dbcbe9f93b5da25dcb8c
Aman Tomar <choudharyamantomar@gmail.com>
2026-07-04 10:58:20 +0530

    Updated MathFxn & Maths Module 2

--- Python Programming Language/Topics/4-0_MathFxn.py
+++ Python Programming Language/Topics/4-0_MathFxn.py
@@ -2,8 +2,8 @@ x = 3.14
 y = -4
 z = 5
 
-print( round (x) )     # Round of decimal
-print( abs (y) )       # Absolute value (distance of a number from 0)
-print( pow (y,z) )     # Power = y to the power z 
-print( max (x, y, z) ) # Finds the max num from given number
-print( min (x, y, z) ) # Finds the min num from given number
+print( "ROUND: ", round (x) )        # Round of decimal
+print( "ABSOLUTE VALUE: ", abs (y) ) # Absolute value (distance of a number from 0)
+print( "POWER: ", pow (y,z) )        # Power = y to the power z 
+print( "MAXm N0.: ", max (x, y, z) ) # Finds the max num from given number
+print( "MINm NO.: ", min (x, y, z) ) # Finds the min num from given number
---
+++ Python Programming Language/Topics/5-2_MathModule(CircumferenceOfACircle).py
@@ -0,0 +1,6 @@
+import math
+
+r = float(input("Enter the Radius of Circle: "))
+c = 2 * math.pi * r
+
+print(f"Circumference of Circle: {round(c, 2)}")
