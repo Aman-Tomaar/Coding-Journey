6e2b2de8181188a882142b28f5f492c3686bb736
Aman Tomar <choudharyamantomar@gmail.com>
2026-07-04 10:33:04 +0530

    Math Module Basics

---
+++ Python Programming Language/Topics/5-0_MathModule.py
@@ -0,0 +1,13 @@
+import math
+
+print(f"VALUE OF pi IN MATHS: {math.pi}\n")
+print(f"VALUE OF e IN PHYSICS: {math.e}\n")
+
+x = int(input("Enter the Number You wanna find out Squareroot of: "))
+print(f"Square Root of {x}: {math.sqrt(x)}\n")
+
+y = float(input("Enter the Number You wanna find out CeilRound of: "))
+print(f"CeilRound of {y}: {math.ceil(y)}\n")
+
+y = float(input("Enter the Number You wanna find out FloorRound of: "))
+print(f"FloorRound of {y}: {math.floor(y)}\n")
