b917ef82eb0ed1528121929772ed4afc8f5f6c57
Aman Tomar <choudharyamantomar@gmail.com>
2026-07-04 10:53:55 +0530

    Math module 1

---
+++ Python Programming Language/Topics/5-1_MathModule(AreaOfACircle).py
@@ -0,0 +1,6 @@
+import math
+
+radius = float(input("Enter the Radius of the Circle: "))
+area = math.pi * radius**2
+
+print(f"Area of the Circle : {round(area, 2)}") #Round fxn used to round the decimal points to 2
