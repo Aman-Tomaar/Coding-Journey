e707c81d7f0a071ef982a41dd269a3cffe5d7b5c
Aman Tomar <choudharyamantomar@gmail.com>
2026-09-04 17:03:03 +0530

    Nested Class

---
+++ Python Programming Language/Topics/36-0_NestedClasses.py
@@ -0,0 +1,32 @@
+#
+
+
+class Company:
+    class Employee:
+        def __init__(self, name, position):
+            self.name = name
+            self.position = position
+
+        def get_details(self):
+            return f"Employee Name: {self.name}, Position: {self.position}"
+
+    def __init__(self, c_name):
+        self.c_name = c_name
+        self.employee = []
+
+    def add_employee(self, name, position):
+        add_employee = self.Employee(name, position)
+        self.employee.append(add_employee)
+
+    def employees_list(self):
+        return [employee.get_details() for employee in self.employee]
+
+
+company = Company("Harley Devidson")
+print(company.c_name)
+
+company.add_employee("William S. Harley", "Founder")
+company.add_employee("Artie Starrs", "CEO")
+
+for x in company.employees_list():
+    print(x)
