e8667696988ab687edf40534abb7404363f4f0c2
Aman Tomar <choudharyamantomar@gmail.com>
2026-09-04 17:08:22 +0530

    Added comment in Nested class

--- Python Programming Language/Topics/36-0_NestedClasses.py
+++ Python Programming Language/Topics/36-0_NestedClasses.py
@@ -1,6 +1,10 @@
+# Nested class = A class defined within another class
+#        class Outer:
+#            class Inner:
 #
-
-
+# Benefits: Allows you to logically group classes that are closely related
+#           Encapsulates private details that aren't relevant outside of the outer class
+#           Keeps the namespace clean; reduces the possibility of naming conflicts
 class Company:
     class Employee:
         def __init__(self, name, position):
