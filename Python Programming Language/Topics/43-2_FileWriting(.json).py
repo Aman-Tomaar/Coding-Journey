7e7bb6f729ea23939a8eca728f7c6482478d8f7e
Aman Tomar <choudharyamantomar@gmail.com>
2026-09-06 06:03:20 +0530

    File Writing .json

---
+++ Python Programming Language/Topics/43-2_FileWriting(.json).py
@@ -0,0 +1,12 @@
+import json
+
+employee = {"name": "Spongebob", "age": 30, "job": "cook"}
+
+file_path = "test.json"
+
+try:
+    with open(file_path, "x") as file:
+        json.dump(employee, file, indent=4)
+        print(f"json file '{file_path}' was created")
+except FileExistsError:
+    print("That file already exists!")
