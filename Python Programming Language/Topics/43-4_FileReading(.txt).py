652328c9fc239193d0eeedc1d8663f9ef4e9768e
Aman Tomar <choudharyamantomar@gmail.com>
2026-09-07 23:57:41 +0530

    File reading Txt file

---
+++ Python Programming Language/Topics/43-4_FileReading(.txt).py
@@ -0,0 +1,10 @@
+file_path = "C:\\Coding\\test.txt"
+
+try:
+    with open(file_path, "r") as file:
+        content = file.read()
+        print(content)
+except FileNotFoundError:
+    print("That file was not found")
+except PermissionError:
+    print("You do not have permission to read that file")
