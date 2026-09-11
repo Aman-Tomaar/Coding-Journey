8211c9810d024660c12e24f6abcca76c0b1caf32
Aman Tomar <choudharyamantomar@gmail.com>
2026-09-11 19:20:08 +0530

    PyQt5 GUI

--- Python Programming Language/Topics/46-0_PyQt5-GUI.py
+++ Python Programming Language/Topics/46-0_PyQt5-GUI.py
@@ -7,15 +7,19 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Strange GUI")
-        self.setGeometry(700, 300, 600, 500)
+        self.setGeometry(
+            700, 300, 600, 500
+        )  # (x, y, width, height) where [x & y] are cordinates if x & y = 0 then it will appare on the top left of the screen
         self.setWindowIcon(QIcon("C:\\Coding\\img.jpg"))
 
 
 def main():
-    app = QApplication(sys.argv)
+    app = QApplication(sys.argv)  # argv means arguments
     window = MainWindow()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(
+        app.exec_()
+    )  # exec_ is execute : waits for user's input and manage clicks, maximise, minimise, exit etc.
 
 
 if __name__ == "__main__":
