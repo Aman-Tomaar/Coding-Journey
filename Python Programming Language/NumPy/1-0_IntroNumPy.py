e4ef1ddac5932e4388c11872b5ee97561be5061c
Aman Tomar <choudharyamantomar@gmail.com>
2026-09-14 21:37:59 +0530

    Intro To Numpy

---
+++ Python Programming Language/NumPy/1-0_IntroNumPy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+array1 = np.array([1, 2, 3, 4])  # Create a NumPy array
+print(array1 * 2)  # Output: [2 4 6 8] - NumPy performs element-wise multiplication
+
+array2 = [1, 2, 3, 4]  # Create a Python list
+print(
+    array2 * 2
+)  # Output: [1, 2, 3, 4, 1, 2, 3, 4] - Python list multiplication repeats the list
