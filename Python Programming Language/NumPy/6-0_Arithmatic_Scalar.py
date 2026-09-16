816978cbbce5284760a912aaf3916e27a8dbec67
Aman Tomar <choudharyamantomar@gmail.com>
2026-09-17 02:40:35 +0530

    Scalar Maths

---
+++ Python Programming Language/NumPy/6-0_Arithmatic_Scalar.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+array = np.array([1, 2, 3, 4, 5])
+print(array)
+print("Scalar Maths")
+print(f"Adding 1 in each element in Array: {array + 1}")
+print(f"Subtracting 2 in each element in Array: {array - 2}")
+print(f"Multiplying 3 with each element in Array: {array * 3}")
+print(f"Dividing 4 from each element in Array: {array / 4}")
+print(f"Putting 5 as the power of each element in Array: {array ** 5}")
