96dfd26d9565c5c77a6c4503ac2089f795992ad6
Aman Tomar <choudharyamantomar@gmail.com>
2026-09-17 02:41:13 +0530

    Vactor Maths

---
+++ Python Programming Language/NumPy/6-1_Arithmatic_Vertorised.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+array = np.array([1.01, 2.3, 3.9, 4.5, 5.4])
+print(f"{array}")
+print("Vactorised Math Functions")
+print(f"Round of Array: {np.round(array)}")
+print(f"Floor Round of Array: {np.floor(array)}")
+print(f"Ceil Round of Array: {np.ceil(array)}")
+print(f"Value of pi: {np.pi}")
